02943fb1899eda288c18537b725b4f8ed2c7f753
Kiyoshi Muranaka <hxhgkkrgi@gmail.com>
2023-10-14 12:54:29 +0900

    Add logger

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import '@/style.css'
 import '@/index.css'
 import App from '@/App.vue'
+import logger from '@/logger'
 import router from '@/router'
 import { createPinia } from 'pinia'
 import { worker } from './mocks/browser'
+
+logger.info('start app')
 
 if (process.env.NODE_ENV === 'development') {
   worker.start()
