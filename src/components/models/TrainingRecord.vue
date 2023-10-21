cf210a5cdc4eec7739010dfc16bc41e0f8a5c293
Kiyoshi Muranaka <hxhgkkrgi@gmail.com>
2023-10-21 11:16:35 +0900

    Add AddTrainingRecord view

---
+++ src/components/models/TrainingRecord.vue
@@ -0,0 +1,11 @@
+<script>
+export class TrainingRecord {
+  constructor(id, userId, trainingItemId, record, date) {
+    this.id = id
+    this.userId = userId
+    this.trainingItemId = trainingItemId
+    this.record = record
+    this.date = date
+  }
+}
+</script>
--- src/components/pages/AddTrainingRecordView.vue
+++ src/components/pages/AddTrainingRecordView.vue
@@ -1,4 +1,20 @@
+<script>
+import AddTrainingRecord from '@/components/containers/AddTrainingRecord.vue'
+
+export default {
+  components: {
+    AddTrainingRecord,
+  },
+}
+</script>
+
 <template>
-  <p>編集モード</p>
-  <input type="text" name="" id="" />
+  <div class="m-4">
+    <div class="flex justify-between">
+      <h1 class="text-4xl">今日の頑張り</h1>
+    </div>
+    <div class="mt-4 ml-4">
+      <AddTrainingRecord />
+    </div>
+  </div>
 </template>
