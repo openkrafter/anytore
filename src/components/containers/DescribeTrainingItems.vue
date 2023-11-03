425f626379ddd28f9ffbb78e738cb1bb5441a040
cumorim <cumorim@gmail.com>
2023-11-03 22:47:53 +0900

    edit modal

--- src/components/containers/DescribeTrainingItems.vue
+++ src/components/containers/DescribeTrainingItems.vue
@@ -3,6 +3,7 @@ import '@/components/containers/DescribeTrainingItems.css'
 import { listTrainingItems } from '@/components/apis/TrainingItem.vue'
 import UpdateTrainingItem from '@/components/containers/UpdateTrainingItem.vue'
 import logger from '@/logger'
+import { Modal } from 'flowbite'
 
 export default {
   components: {
@@ -44,6 +45,26 @@ export default {
   methods: {
     updateTrainingItem() {
       logger.trace('update training!')
+      const $targetEl = document.getElementById('update-training-item-modal1')
+      const options = {
+        placement: 'bottom-right',
+        backdrop: 'dynamic',
+        backdropClasses:
+          'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+        closable: true,
+        onHide: () => {
+          console.log('modal is hidden')
+        },
+        onShow: () => {
+          console.log('modal is shown')
+        },
+        onToggle: () => {
+          console.log('modal has been toggled')
+        },
+      }
+
+      const modal = new Modal($targetEl, options)
+      modal.toggle()
     },
   },
 
@@ -83,6 +104,7 @@ export default {
             d="m14.258 7.985-3.025 3.025A3 3 0 1 1 6.99 6.768l3.026-3.026A3.01 3.01 0 0 1 8.411 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V9.589a3.011 3.011 0 0 1-1.742-1.604Z"
           />
         </svg>
+        <slot />
       </button>
 
       <div
