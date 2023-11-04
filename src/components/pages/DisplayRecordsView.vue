d4087aad21848b0a7647406f2fe2b5b83c228bb0
山田正史 <masabon@yamadaseishinoMacBook-Air.local>
2023-11-04 10:10:29 +0900

    init DisplayRecordsView

--- src/components/pages/DisplayRecordsView.vue
+++ src/components/pages/DisplayRecordsView.vue
@@ -1,47 +1,13 @@
 <script>
-import TodoAdd from '@/components/containers/TodoAdd.vue'
-import TodoList from '@/components/containers/TodoList.vue'
-import { mapActions, mapStores } from 'pinia'
-import { useTodosStore } from '@/stores/todos'
-
 export default {
-  components: {
-    TodoAdd,
-    TodoList,
-  },
-
-  computed: {
-    ...mapStores(useTodosStore),
-  },
-
-  methods: {
-    ...mapActions(useTodosStore, ['addTodo', 'clearDoneTodos']),
-
-    addNewTodo(newTodoText) {
-      if (!newTodoText) return alert('文字を入力してください')
-
-      if (newTodoText === '') {
-        alert('文字を入力してください')
-        return
-      }
-
-      this.addTodo({
-        isDone: false,
-        text: newTodoText,
-      })
-    },
+  data() {
+    return {
+      sampleData: {},
+    }
   },
 }
 </script>
 
 <template>
-  <TodoAdd @delete-done="clearDoneTodos" @add-todo="addNewTodo" />
-  <p v-if="todosStore.todos.length === 0">ToDoがまだありません！</p>
-  <TodoList v-else :todos="todosStore.todos" />
+  <p>グラフを表示する</p>
 </template>
-
-<style>
-.todo-done {
-  text-decoration: line-through;
-}
-</style>
